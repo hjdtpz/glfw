811cb58879c96516fdb751ac00e0598b768479d8
hjdtpz <hjdtpz@outlook.com>
2024-04-27 10:19:06 +0800

    premake

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,7 @@
 ﻿project "GLFW"
     kind "StaticLib"
     language "C"
-    staticruntime "on"
+    staticruntime "off"
     
 	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
 	objdir("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
