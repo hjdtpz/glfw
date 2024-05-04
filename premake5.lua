985efb2d81ab5c89b9056fd9d9e87fa7c53daa3d
hjdtpz <hjdtpz@outlook.com>
2024-05-04 17:04:39 +0800

    fix crt

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,7 @@
-﻿project "GLFW"
+project "GLFW"
     kind "StaticLib"
     language "C"
-    staticruntime "off"
+    staticruntime "on"
     
 	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
 	objdir("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
